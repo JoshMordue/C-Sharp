7aaa4720135b150667786cdca072a38d1c28bdcb
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-05-26 00:47:14 +0100

    work on course

---
+++ ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 9; i++) 
+            {
+                Console.Write($"Row {i}: ");
+                for (int j = 0; j <= 9; j++)
+
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }             
+        }
+    }
+}
+4
