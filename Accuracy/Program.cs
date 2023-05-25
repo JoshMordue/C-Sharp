207aaf3aeb8ab9aa166124d6f53b172b83fd1200
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-05-25 19:39:36 +0100

    work on course

--- Accuracy/Program.cs
+++ Accuracy/Program.cs
@@ -8,13 +8,22 @@ namespace Accuracy
         static void Main(string[] args)
         {
             Console.Clear();
-            double result1 = 0.1 * 8;
-            double result2 = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+            //double result1 = 0.1 * 7;
+            //double result2 = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+            //Console.WriteLine();
+            //Console.WriteLine($"first result is: {result1:R}");
+            //Console.WriteLine($"second result is: {result2:R}");
+
+            //double difference = result1 - result2;
+            //Console.WriteLine($"the difference is {difference:R}");
+
+            decimal result1 = 0.1m * 7m;
+            decimal result2 = 0.1m + 0.1m + 0.1m + 0.1m + 0.1m + 0.1m + 0.1m;
             Console.WriteLine();
             Console.WriteLine($"first result is: {result1}");
             Console.WriteLine($"second result is: {result2}");
 
-            double difference = result1 - result2;
+            decimal difference = result1 - result2;
             Console.WriteLine($"the difference is {difference}");
 
         }
---
+++ BoolExp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace BoolExp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int apples = 6;
+            int oranges = 9;
+            decimal applePrice = 12.60m;
+            decimal orangePrice = 4.50m;
+
+            //Console.WriteLine(apples == oranges);
+            //Console.WriteLine(apples != oranges);
+
+            //Console.WriteLine($"{apples} > {oranges} is {apples > oranges}");
+            //Console.WriteLine($"{apples} < {oranges} is {apples < oranges}");
+            //Console.WriteLine($"{apples} >= {oranges} is {apples >= oranges}");
+            //Console.WriteLine($"{apples} <= {oranges} is {apples <= oranges}");
+            //Console.WriteLine($"{apples} is int is {apples is int}");
+
+
+            //Console.WriteLine($"Reducing apple cost: {(apples > oranges) && (applePrice > orangePrice)}");
+            //Console.WriteLine($"Reducing apple cost: {(apples > oranges) || (applePrice > orangePrice)}");
+
+
+            bool moreApples;
+            bool applesAreDearer;
+
+            moreApples = (apples > oranges);
+            applesAreDearer = (applePrice > orangePrice);
+
+            Console.WriteLine($"We have have more apples: {moreApples}");
+            Console.WriteLine($"Apples are dearer: {applesAreDearer}");
+
+            Console.WriteLine($"Reducing Apple Cost: {moreApples && applesAreDearer}");
+            Console.WriteLine($"Reducing apple cost: {moreApples || applesAreDearer}");
+        }
+    }
+}
