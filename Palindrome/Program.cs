9431666bfdbfa16bebe7f08f08a6a0805f38974f
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-06-02 23:50:48 +0100

    work on a challenge

---
+++ Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        private static string inputText;
+        bool isPalindrome;
+
+
+        static void Main(string[] args, bool isPalindrome)
+        {
+            System.Console.WriteLine("****************");
+            Console.WriteLine("Please enter a word/Sentence and we see if it is a palindrome");
+            while (true) {
+                inputText = Console.ReadLine();
+                if (inputText != null || Regex.IsMatch(inputText, @"^[a-zA-Z]+$ "));
+                {
+                    isPalindrome = inputCheck(inputText);
+                }
+                
+            }
+        }
+
+        public string inputCheck(string Text) { 
+            Text = Text.Trim(' ');
+            Console.WriteLine(Text);
+        }
+    }
+}
