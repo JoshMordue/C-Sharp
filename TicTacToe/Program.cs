2ad6071796f6fd4a2652679934362797f02db467
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-06-10 19:27:35 +0100

    work on project

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,30 +6,47 @@ namespace TicTacToe
     internal class Program
     {
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-        static int player = 1;
+        static int player = 1; //player 1 is X, player 2 is O
         static int choice;
 
         static int winner = 0;
 
         static void Main(string[] args)
         {
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("TIC-TAC-TOE");
-                Console.WriteLine("Player 1 is 'X', Player 2 is 'O'");
-                Console.WriteLine("*****************************");
-                Board();
-            } while (winner == 0);
-            {
-                Board();
-                Console.WriteLine("\n");
-                Console.WriteLine($"Player: {player} turn!");
-                Console.WriteLine("Please enter your choice");
-                Console.WriteLine("\n");
 
+            while (winner == 0) {
+                Board();
+                Console.WriteLine("\n");
+                Console.WriteLine($"Player {player}'s turn!");
+                Console.WriteLine("Please enter your choice");
                 choice = int.Parse(Console.ReadLine());
 
+                if (arr[choice] != 'X' && arr[choice] != 'O')
+                {
+                    if (player == 1)
+                    {
+                        arr[choice] = 'X';
+                        player++;
+                    }
+                    else
+                    {
+                        arr[choice] = 'O';
+                        player--;
+                    }
+
+                }
+                else
+                {
+                    Console.Write($"Unfortunately space {choice} has already been taken.");
+                    Console.WriteLine("\n");
+                    Console.Write("Please ensure you pick a spot that's available.");
+                    Console.WriteLine("\n");
+                    Thread.Sleep(2000);
+
+
+                }
+
+                Console.Clear();
 
             }
 
@@ -45,6 +62,10 @@ namespace TicTacToe
 
         private static void Board()
         {
+            Console.WriteLine("TIC-TAC-TOE");
+            Console.WriteLine("*****************************");
+            Console.WriteLine("Player 1 is 'X', Player 2 is 'O'");
+
             Console.WriteLine("     |     |      ");
             Console.WriteLine($"  {arr[1]}  |  {arr[2]}  |  {arr[3]}  ");
             Console.WriteLine("_____|_____|_____ ");
