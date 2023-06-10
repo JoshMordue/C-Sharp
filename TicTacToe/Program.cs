ae3c0f6a62e928abf393811d90ade30c125da207
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-06-10 18:41:26 +0100

    work on project

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+
+        static void Main(string[] args)
+        {
+            Board();
+        }
+
+
+
+
+        private static void Board()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine($"  {arr[1]}  |  {arr[2]}  |  {arr[3]}  ");
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine($"  {arr[4]}  |  {arr[5]}  |  {arr[6]}  ");
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine($"  {arr[7]}  |  {arr[8]}  |  {arr[9]}  ");
+            Console.WriteLine("     |     |      ");
+
+
+
+
+
+        }
+    }
+}
