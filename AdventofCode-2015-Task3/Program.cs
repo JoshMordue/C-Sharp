e17c8d687fd9fcf8f32b0e311d98b3ad2775a410
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-06-13 17:14:10 +0100

    work on challenge

---
+++ AdventofCode-2015-Task3/Program.cs
@@ -0,0 +1,85 @@
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode_2015_Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"prompt.txt";
+            int x = 0;
+            int y = 0;
+            int rx = 0;
+            int ry = 0;
+            int turn = 0;
+            string combination;
+            List<string> uniqueLocations = new List<string>();
+
+
+            string fs = File.ReadAllText(path);
+
+            foreach (char direction in fs)
+            {
+
+
+                if (turn == 0) 
+                {
+                    if (direction == '^')
+                    {
+                        y++;
+                    }
+                    else if (direction == 'v')
+                    {
+                        y--;
+                    }
+                    else if (direction == '>')
+                    {
+                        x++;
+                    }
+                    else
+                    {
+                        x--;
+                    }
+
+                    combination = (x.ToString() + y.ToString());
+                    turn++;
+                }
+                else
+                {
+                    if (direction == '^')
+                    {
+                        ry++;
+                    }
+                    else if (direction == 'v')
+                    {
+                        ry--;
+                    }
+                    else if (direction == '>')
+                    {
+                        rx++;
+                    }
+                    else
+                    {
+                        rx--;
+                    }
+                    combination = (rx.ToString() + ry.ToString());
+                    turn--;
+                }
+
+
+                uniqueLocations.Add(combination);
+
+                int distinctCount = uniqueLocations.Distinct().Count() + 1;
+
+
+                Console.WriteLine(distinctCount);
+            }
+        }
+    }
+}
