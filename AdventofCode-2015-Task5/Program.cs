55729d32bee4d01517c049a9b0f8546efa427ff2
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-06-14 18:14:48 +0100

    please don't brick my pc

--- AdventofCode-2015-Task5/Program.cs
+++ AdventofCode-2015-Task5/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace AdventofCode_2015_Task5
 {
@@ -22,58 +17,61 @@ namespace AdventofCode_2015_Task5
                 while ((line = sentences.ReadLine()) != null)
                 {
 
-                    if (CheckLine(line) == true) {
-                        
+                    if (CheckLine(line))
+                    {
                         counter++;
                     }
-                   
+
                 }
                 Console.WriteLine(counter);
 
             }
         }
 
-        static bool CheckLine(string line)
+         static bool CheckLine(string line)
         {
             string[] illegalStrings = { "ab", "cd", "pq", "xy" };
             int vowelCount = 0;
 
             //checking for illegal strings
-            if (illegalStrings.All(line.Contains))
-            {
-                return false;
-            }
 
-            for (int i = 0; i < line.Length - 1; i++)
+            foreach (var nonoword in illegalStrings)
             {
-                    //check for duplicate letters
-                    if (line[i] == line[i + 1])
+                if (line.Contains(nonoword))
+                {
+                    Console.WriteLine($"{nonoword}");
+                    return false;
+                }
+            } 
+
+
+            for (int i = 0; i < line.Length; i++)
+            {         
+                if (i > 0)
+                {
+                    if (line[i] == line[i - 1])
                     {
                         return false;
-
                     }
-                
+                }
+
                 //checking for 3 vowels minimum
-                else if (vowelCount < 3)
+                if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' || line[i] == 'o' || line[i] == 'u')
                 {
-                    if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' || line[i] == 'o' || line[i] == 'u')
-                    {
-                        vowelCount++;
-                        Console.WriteLine("test3");
-
-                        if (vowelCount > 10)
-                        {
-                            Console.WriteLine("test4");
-                            return true;
-                        }
-                    }
-
+                    vowelCount++;
                 }
             }
-                return false;
-            }
 
+            if (vowelCount > 2)
+            {
+                return true;
+            }
+            return false;
         }
+
     }
-}
+
+} 
+
+
 
