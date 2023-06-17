d852c2dd86768517865ed7bcc8f80be2f937034c
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-06-17 22:48:21 +0100

    work on challenge

---
+++ AdventofCode-2016-Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventofCode_2016_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"prompt.txt";
+
+            
+
+
+        }
+    }
+}
---
+++ FirstFactorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class MainClass
+{
+
+    public static int FirstFactorial(int num)
+    {
+
+        // code goes here  
+        int answer = 0;
+
+        while (num < 0)
+    {
+            answer *= num;
+            num--;
+        }
+        return answer.ToString();
+
+    }
+
+    static void Main()
+    {
+
+        // keep this function call here
+        Console.WriteLine(FirstFactorial(Console.ReadLine()));
+
+    }
+
+}
