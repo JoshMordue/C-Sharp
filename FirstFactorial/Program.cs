c84130775441401eef98e046058f98411fd50b74
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-06-18 22:50:24 +0100

    work on challenge

--- FirstFactorial/Program.cs
+++ FirstFactorial/Program.cs
@@ -5,7 +5,7 @@ class MainClass
 
     public static int FirstFactorial(int num)
     {
-
+        // displays the factorial numbers of the int provided 
         // code goes here  
         int answer = 0;
 
