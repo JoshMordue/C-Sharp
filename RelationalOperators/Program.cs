b18cf9ffe0c42891694301a53ab619aa8de9cea2
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-07-04 21:54:04 +0100

    work on the course

---
+++ RelationalOperators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationalOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds = 31;
+            int minutes = 1;
+
+            if ((minutes < 59) && ((seconds + 1) > 59))
+            {
+                minutes++;
+            }
+        }
+    }
+}
