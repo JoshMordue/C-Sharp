5d7b635645cb3d04b16109ca12f6c09759c21afe
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-07-16 23:06:52 +0100

    work on course

---
+++ StringExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string courseName = "Learn C# for beginners Crash Course";
+            string message = "Welcome to ";
+
+            // Concatonate CourseName to create a new string.
+
+            string fullMessage = message + courseName;
+            Console.WriteLine(fullMessage);
+
+            //use string interpolation 
+            string fullMessage2 = $"{message}{courseName}";
+            Console.WriteLine(fullMessage2);
+
+        }
+    }
+}
