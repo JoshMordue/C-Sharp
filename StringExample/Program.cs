2713c1e5f9022f49fbc81fe7f640935d1cb4fb50
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-07-16 23:35:33 +0100

    work on course

--- StringExample/Program.cs
+++ StringExample/Program.cs
@@ -11,17 +11,38 @@ namespace StringExample
         static void Main(string[] args)
         {
             string courseName = "Learn C# for beginners Crash Course";
-            string message = "Welcome to ";
 
-            // Concatonate CourseName to create a new string.
 
-            string fullMessage = message + courseName;
-            Console.WriteLine(fullMessage);
+            int position;
 
-            //use string interpolation 
-            string fullMessage2 = $"{message}{courseName}";
-            Console.WriteLine(fullMessage2);
+            position = courseName.IndexOf("c", StringComparison.OrdinalIgnoreCase);
 
+            Console.WriteLine(position);
+
+
+
+
+
+            //string message = "Welcome to ";
+
+            //// Concatonate CourseName to create a new string.
+
+            //string fullMessage = message + courseName;
+            //Console.WriteLine(fullMessage);
+
+            ////use string interpolation 
+            //string fullMessage2 = $"{message}{courseName}";
+            //Console.WriteLine(fullMessage2);
+
+
+            ////use composite formatting with methods that support it
+            //Console.WriteLine("Hello, and {0} the {1}", message, courseName);
+
+            //for (int i = 0; i < courseName.Length; i++)
+            //{
+            //    char character = courseName[i];
+            //    Console.WriteLine(character);
+            //}
         }
     }
 }
