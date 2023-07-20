22a5db4d37cbac1f282032ffdba9decc0e0d8349
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-07-20 23:11:27 +0100

    work on course

---
+++ ImmutableStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImmutableStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = x;
+
+            Console.WriteLine($"x = {x}, y = {y}");
+
+            Console.WriteLine($"x is the same as y: {object.ReferenceEquals(x, y)}");
+        }
+    }
+}
