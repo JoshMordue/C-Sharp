18de34bd851f006e717f737877c7610587ab0fc4
JoshMordue <joshua.mordue@hotmail.co.uk>
2023-07-23 22:00:49 +0100

    work on course

--- ImmutableStrings/Program.cs
+++ ImmutableStrings/Program.cs
@@ -12,23 +12,22 @@ namespace ImmutableStrings
         static void Main(string[] args)
         {
             StringBuilder first = new StringBuilder("This is a string");
-            StringBuilder second = first;
+            StringBuilder second = new StringBuilder("This is a string");
 
             Console.WriteLine($"First:  {first}");
-            Console.WriteLine($"Second:  {second}");
-            Console.WriteLine($"First is the same as second: {object.ReferenceEquals(first, second)}");
+            Console.WriteLine("Clearing First...");
 
-            Console.WriteLine();
+            //first.Length = 0;
+            //Console.WriteLine($"First:  *{first}*");
 
-            first = first.Remove(4, 5);
-            //first = first.Insert(4, " is a");
-            Console.WriteLine($"First:  {first}");
-            Console.WriteLine($"Second:  {second}");
-            Console.WriteLine();
-            Console.WriteLine($"First is the same as second: {object.ReferenceEquals(first, second)}");
-            Console.WriteLine($"first == second: {first == second}");
-            Console.WriteLine($"First .Equals second: {first.Equals(second)}");
+            //first.Append("Another string");
+            first.Clear().Append("Another String");
+            first.Clear();
+            second.Clear();
 
+            Console.WriteLine(first == second); 
+
+            Console.WriteLine($"First:  *{first}*");
 
 
 
